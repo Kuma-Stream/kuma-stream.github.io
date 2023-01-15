3063f9e4fd62a0d5b9635b498b6a98b395d550e6
Kuma-Stream <121804937+Kuma-Stream@users.noreply.github.com>
2023-01-15 23:17:07 +0700

    Create ep.js

---
+++ anime/saikyo-on/ep.js
@@ -0,0 +1,8 @@
+var player = new Playerjs({
+    id: "1",
+    file: "[360p]//kumaa.my.id/0:/Saikyou%20Onmyouji%20no%20Isekai%20Tenseiki/1/akuma_SOIT--01_360p.mp4,[480p]//kumaa.my.id/0:/Saikyou%20Onmyouji%20no%20Isekai%20Tenseiki/1/akuma_SOIT--01_480p.mp4,[720p]//kumaa.my.id/0:/Saikyou%20Onmyouji%20no%20Isekai%20Tenseiki/1/akuma_SOIT--01_720p.mp4"
+  })
+  new Playerjs({
+    id: "2",
+    file: "[480p]//kumaa.my.id/0:/Saikyou%20Onmyouji%20no%20Isekai%20Tenseiki/2/akuma%20-%20SkyouOnmyjinIskaiTnski.02.480p.mp4,[720p]//kumaa.my.id/0:/Saikyou%20Onmyouji%20no%20Isekai%20Tenseiki/2/akuma%20-%20SkyouOnmyjinIskaiTnski.02.720p.mp4,[1080p]//kumaa.my.id/0:/Saikyou%20Onmyouji%20no%20Isekai%20Tenseiki/2/akuma%20-%20SkyouOnmyjinIskaiTnski.02.1080p.mp4"
+  })
