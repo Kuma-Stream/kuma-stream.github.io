d700699f64b9f6e14793e3a2401d01e44f1df3a8
Kuma-Stream <121804937+Kuma-Stream@users.noreply.github.com>
2023-01-16 02:27:31 +0700

    Create ep.js

---
+++ anime/nier/ep.js
@@ -0,0 +1,8 @@
+    var player = new Playerjs({
+    id: "1",
+    file: "[360p]//kumaa.my.id/0:/NieR:Automata%20Ver1.1a/1/mp4/akuma-NieR-01-360p-SAME.mp4,[480p]//kumaa.my.id/0:/NieR:Automata%20Ver1.1a/1/mp4/akuma-NieR-01-480p-SAME.mp4,[720p]//kumaa.my.id/0:/NieR:Automata%20Ver1.1a/1/mp4/akuma-NieR-01-MP4HD-SAME.mp4,[1080p]//kumaa.my.id/0:/NieR:Automata%20Ver1.1a/1/mp4/akuma-NieR-01-FULLHD-SAME.mp4"
+  })
+  var player = new Playerjs({
+    id: "2",
+    file: "[360p]//kumaa.my.id/0:/NieR:Automata%20Ver1.1a/2/akuma%20-%20NieR-02-360p-SAME.mp4,[480p]//kumaa.my.id/0:/NieR:Automata%20Ver1.1a/2/akuma%20-%20NieR-02-480p-SAME.mp4,[720p]//kumaa.my.id/0:/NieR:Automata%20Ver1.1a/2/akuma%20-%20NieR-02-MP4HD-SAME.mp4,[1080p]//kumaa.my.id/0:/NieR:Automata%20Ver1.1a/2/akuma%20-%20NieR-02-FULLHD-SAME.mp4"
+  })
