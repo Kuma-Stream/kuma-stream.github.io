c73db7bc1cbee7af16c1fe1f206a4dfb5f2b1f65
Kuma-Stream <121804937+Kuma-Stream@users.noreply.github.com>
2023-01-15 22:40:21 +0700

    Update ep.js

--- anime/saikyo-on/ep.js
+++ anime/saikyo-on/ep.js
@@ -1,8 +1,1 @@
-var player = new Playerjs({
-    id: "1",
-    file: "[360p]//kumaa.my.id/0:/Saikyou%20Onmyouji%20no%20Isekai%20Tenseiki/1/akuma_SOIT--01_360p.mp4,[480p]//kumaa.my.id/0:/Saikyou%20Onmyouji%20no%20Isekai%20Tenseiki/1/akuma_SOIT--01_480p.mp4,[720p]//kumaa.my.id/0:/Saikyou%20Onmyouji%20no%20Isekai%20Tenseiki/1/akuma_SOIT--01_720p.mp4"
-  })
-  new Playerjs({
-    id: "2",
-    file: "[480p]//kumaa.my.id/0:/Saikyou%20Onmyouji%20no%20Isekai%20Tenseiki/2/akuma%20-%20SkyouOnmyjinIskaiTnski.02.480p.mp4,[720p]//kumaa.my.id/0:/Saikyou%20Onmyouji%20no%20Isekai%20Tenseiki/2/akuma%20-%20SkyouOnmyjinIskaiTnski.02.720p.mp4,[1080p]//kumaa.my.id/0:/Saikyou%20Onmyouji%20no%20Isekai%20Tenseiki/2/akuma%20-%20SkyouOnmyjinIskaiTnski.02.1080p.mp4"
-  })
+dmFyIHBsYXllciA9IG5ldyBQbGF5ZXJqcyh7CiAgICBpZDogIjEiLAogICAgZmlsZTogIlszNjBwXS8va3VtYWEubXkuaWQvMDovU2Fpa3lvdSUyME9ubXlvdWppJTIwbm8lMjBJc2VrYWklMjBUZW5zZWlraS8xL2FrdW1hX1NPSVQtLTAxXzM2MHAubXA0LFs0ODBwXS8va3VtYWEubXkuaWQvMDovU2Fpa3lvdSUyME9ubXlvdWppJTIwbm8lMjBJc2VrYWklMjBUZW5zZWlraS8xL2FrdW1hX1NPSVQtLTAxXzQ4MHAubXA0LFs3MjBwXS8va3VtYWEubXkuaWQvMDovU2Fpa3lvdSUyME9ubXlvdWppJTIwbm8lMjBJc2VrYWklMjBUZW5zZWlraS8xL2FrdW1hX1NPSVQtLTAxXzcyMHAubXA0IgogIH0pCiAgbmV3IFBsYXllcmpzKHsKICAgIGlkOiAiMiIsCiAgICBmaWxlOiAiWzQ4MHBdLy9rdW1hYS5teS5pZC8wOi9TYWlreW91JTIwT25teW91amklMjBubyUyMElzZWthaSUyMFRlbnNlaWtpLzIvYWt1bWElMjAtJTIwU2t5b3VPbm15amluSXNrYWlUbnNraS4wMi40ODBwLm1wNCxbNzIwcF0vL2t1bWFhLm15LmlkLzA6L1NhaWt5b3UlMjBPbm15b3VqaSUyMG5vJTIwSXNla2FpJTIwVGVuc2Vpa2kvMi9ha3VtYSUyMC0lMjBTa3lvdU9ubXlqaW5Jc2thaVRuc2tpLjAyLjcyMHAubXA0LFsxMDgwcF0vL2t1bWFhLm15LmlkLzA6L1NhaWt5b3UlMjBPbm15b3VqaSUyMG5vJTIwSXNla2FpJTIwVGVuc2Vpa2kvMi9ha3VtYSUyMC0lMjBTa3lvdU9ubXlqaW5Jc2thaVRuc2tpLjAyLjEwODBwLm1wNCIKICB9KQo=
