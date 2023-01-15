26c8312c33c37965686aee6fe0a9ae92a568263e
Kuma-Stream <121804937+Kuma-Stream@users.noreply.github.com>
2023-01-16 02:00:44 +0700

    Create ep.js

---
+++ anime/bnriya/ep.js
@@ -0,0 +1,9 @@
+var player = new Playerjs({
+    id: "1",
+    file: "[360p]//kumaa.my.id/0:/Benriya%20Saitou-san%20Isekai%20ni%20Iku/1/akuma-BENTOU-01-360p-BGlobal.mp4,[480p]//kumaa.my.id/0:/Benriya%20Saitou-san%20Isekai%20ni%20Iku/1/akuma-BENTOU-01-480p-BGlobal.mp4,[720p]//kumaa.my.id/0:/Benriya%20Saitou-san%20Isekai%20ni%20Iku/1/akuma-BENTOU-01-720p-BGlobal.mp4"
+  })
+  
+var player = new Playerjs({
+    id: "2",
+    file: "[360p]//kumaa.my.id/0:/Benriya%20Saitou-san%20Isekai%20ni%20Iku/2/akuma-BENTOU-02-360p-BGlobal.mp4,[480p]//kumaa.my.id/0:/Benriya%20Saitou-san%20Isekai%20ni%20Iku/2/akuma-BENTOU-02-480p-BGlobal.mp4,[720p]//kumaa.my.id/0:/Benriya%20Saitou-san%20Isekai%20ni%20Iku/2/akuma-BENTOU-02-720p-BGlobal.mp4"
+  })
